e4261ac18655f15e0e333ee038990fd9e47c8723
Loner1024 <xth12138@gmail.com>
2021-01-17 22:27:06 +0800

    递归实现

--- 203.移除链表元素.go
+++ 203.移除链表元素.go
@@ -11,27 +11,42 @@
  *     Next *ListNode
  * }
  */
+
+// 循环实现
+// func removeElements(head *ListNode, val int) *ListNode {
+// 	if head == nil {
+// 		return head
+// 	}
+// 	for head.Val == val && head != nil {
+// 		head = head.Next
+// 		if head == nil {
+// 			return head
+// 		}
+// 	}
+// 	pre := head
+// 	for pre.Next != nil {
+// 		if pre.Next.Val == val {
+// 			delNode := pre.Next
+// 			pre.Next = delNode.Next
+// 			delNode.Next = nil
+// 		} else {
+// 			pre = pre.Next
+// 		}
+// 	}
+// 	return head
+// }
+
+// 递归实现
 func removeElements(head *ListNode, val int) *ListNode {
 	if head == nil {
 		return head
 	}
-	for head.Val == val && head != nil {
-		head = head.Next
-		if head == nil {
-			return head
-		}
+	if head.Val == val {
+		return removeElements(head.Next, val)
+	} else {
+		head.Next = removeElements(head.Next, val)
+		return head
 	}
-	pre := head
-	for pre.Next != nil {
-		if pre.Next.Val == val {
-			delNode := pre.Next
-			pre.Next = delNode.Next
-			delNode.Next = nil
-		} else {
-			pre = pre.Next
-		}
-	}
-	return head
 }
 
 // @lc code=end
