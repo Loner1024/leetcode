432b59b4d14da58168b66dbb931c6440a8520543
Loner1024 <xth12138@gmail.com>
2021-01-19 01:01:37 +0800

    [21] 合并两个有序链表

--- 21.合并两个有序链表.go
+++ 21.合并两个有序链表.go
@@ -28,13 +28,15 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	}
 	for l1 != nil {
 		cur.Next = l1
-		cur = cur.Next
-		l1 = l1.Next
+		l1 = nil
+		// cur = cur.Next
+		// l1 = l1.Next
 	}
 	for l2 != nil {
 		cur.Next = l2
-		cur = cur.Next
-		l2 = l2.Next
+		l2 = nil
+		// cur = cur.Next
+		// l2 = l2.Next
 	}
 	return dummy.Next
 }
